b5079a1493a99372123cdf960bf159d377f224d3
Arthur Léothaud <arthur@leothaud.eu>
2020-12-29 22:19:53 +0100

    change license

--- flood.el
+++ flood.el
@@ -10,15 +10,19 @@
 
 ;; This file is not part of GNU Emacs
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+;; Version 2, December 2004
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; Copyright (C) 2020 Arthur Léothaud <arthur@leothaud.eu>
+
+;; Everyone is permitted to copy and distribute verbatim or modified
+;; copies of this license document, and changing it is allowed as long
+;; as the name is changed.
+
+;; DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+;; TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+;; 0. You just DO WHAT THE FUCK YOU WANT TO.
 
 ;; For a full copy of the GNU General Public License
 ;; see <http://www.gnu.org/licenses/>.
